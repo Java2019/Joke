f5f17a7828c57c4f84bcbe602ab12a77151c4b00
java2019 <java_2019@mail.ru>
2017-02-10 14:46:16 +0300

    Steps

---
+++ app/src/main/java/com/example/use/joke/DelayedMessageService.java
@@ -0,0 +1,18 @@
+package com.example.use.joke;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+
+public class DelayedMessageService extends IntentService {
+
+    public DelayedMessageService() {
+        super("DelayedMessageService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
+
+}
